05e6b4647d86991d0e178e61c9f4870de745f160
Vincent PIERRO <vincent.pierro@efrei.net>
2025-10-18 23:36:49 +0200

    ADD(DashboardController)

--- src/main/java/ApplicationDeSuiviDeTutorat/controller/DashboardController.java
+++ src/main/java/ApplicationDeSuiviDeTutorat/controller/DashboardController.java
@@ -1,4 +1,23 @@
 package ApplicationDeSuiviDeTutorat.controller;
 
+import ApplicationDeSuiviDeTutorat.service.DashboardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+import java.security.Principal;
+
+@Controller
 public class DashboardController {
+    private final DashboardService dashboardService;
+
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping("/dashboard")
+    public String Dashboard(Model model) {
+        model.addAttribute("trainees", dashboardService.getTrainees());
+        return "dashboard";
+    }
 }
