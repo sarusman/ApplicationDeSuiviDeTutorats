a5f6ca1d8e00988e910950a4f3940460e289233d
sarusman <sarusmandu92@gmail.com>
2025-10-30 15:07:59 +0100

    Exceptions paufinage

--- src/main/java/ApplicationDeSuiviDeTutorat/Exceptions/DuplicateEmailException.java
+++ src/main/java/ApplicationDeSuiviDeTutorat/Exceptions/DuplicateEmailException.java
@@ -1,5 +1,8 @@
 package ApplicationDeSuiviDeTutorat.Exceptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateEmailException extends RuntimeException {
     public DuplicateEmailException(String message) { super(message); }
 }
--- src/main/java/ApplicationDeSuiviDeTutorat/Exceptions/DuplicateUsernameException.java
+++ src/main/java/ApplicationDeSuiviDeTutorat/Exceptions/DuplicateUsernameException.java
@@ -1,5 +1,9 @@
 package ApplicationDeSuiviDeTutorat.Exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
 public class DuplicateUsernameException extends RuntimeException {
     public DuplicateUsernameException(String message) { super(message); }
 }
